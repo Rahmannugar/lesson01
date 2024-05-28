c023eac6b6ba988352827fdc0fe04da42f943849
Nugar <cladeadenugar@gmail.com>
2024-05-28 02:11:24 +0100

    new

--- build/js/main.js
+++ build/js/main.js
@@ -3,9 +3,20 @@ let myName;
 let age;
 let isLoading;
 let what;
+let album;
 myName = "Nugar";
 age = 21;
 isLoading = true;
 what = 33;
 what = "what";
+album = 2003;
+album = "Here";
 console.log(what);
+console.log(album);
+const sum = (a, b) => {
+    return a + b;
+};
+let postId;
+let isActive;
+let re = /\w+/g;
+//Array and objects
--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,27 @@ let myName: string;
 let age: number;
 let isLoading: boolean;
 let what: any;
+let album: string | number;
 
 myName = "Nugar";
 age = 21;
 isLoading = true;
 what = 33;
 what = "what";
+album = 2003;
+album = "Here";
+
 console.log(what);
+console.log(album);
+
+const sum = (a: number, b: string) => {
+  return a + b;
+};
+
+let postId: string | number;
+let isActive: number | boolean;
+
+let re: RegExp = /\w+/g;
+
+
+//Array and objects
